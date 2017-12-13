fa77829730cbbdb0e2390550581a765979aa5137
mqhasan <34293551+mqhasan@users.noreply.github.com>
2017-12-13 17:11:40 -0500

    uploaded file with updated hardcoded contract

--- contracts/TaxCollection.sol
+++ contracts/TaxCollection.sol
@@ -2,17 +2,22 @@ pragma solidity ^0.4.11;
 
 contract TaxCollection{
 
-    address citizen;
-    mapping(address => uint) public balances;
+    address public dept1 = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
+    uint deptSplit = 30;
+    uint departmentSplit;
+    address sender;
+    uint amount;
+    event sendSplit (address dept1, uint Split);
 
-    function TaxCollection(uint _amount) {
-         citizen = msg.sender;
-         balances[citizen] = _amount;
+    mapping(address => uint) public departmentAmount;
+
+    function TaxCollection() {
+        sender = msg.sender;
     }
-}
 
-    function payDepartment(){
-        
-    } 
-   
+    function payDepartment() return (uint) {
+        amount=msg.value;
+        departmentSplit= (amount*deptSplit)/100;
+        return (departmentSplit)
+    }
 }
