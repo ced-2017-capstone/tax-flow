1a7b8428234f188690c41be6d623dc85dc3f86b9
shaanray <shaanrayoffice@gmail.com>
2017-12-14 10:34:25 -0500

    added two functions to contract Invoice, the first is invoice generation and the second is sending the actual invoice

--- contracts/Invoice.sol
+++ contracts/Invoice.sol
@@ -1,7 +1,24 @@
 pragma solidity ^0.4.11;
 
-contract Invoice { 
+contract Invoice {
+
     function Invoice (){
-        
+        sender = msg.sender;
+        balance = 0;
+        pending = 100;
     }
+ // balance is 0 until payment arrives
+
+    address public owner;
+    uint public balance;
+
+    mapping(address => uint) Department;
+
+    function sendInvoice(address Department) onlyBy(owner){
+        sender = msg.sender;
+        pending = 100;
+    }
+ // supplier sends invoice to the Department
+
+
 }
